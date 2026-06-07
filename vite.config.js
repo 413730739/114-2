dcae7a51637759ef9ad07d7c7de24731a0cf3bd5
413730739 <413730739@o365.tku.edu.tw>
2026-06-07 18:52:19 +0800

    英文

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   // 這裡的名稱必須與你在 GitHub 上的 Repository (倉庫) 名稱一致
   // 例如你的倉庫叫 interactive-english-system
-  base: '/你的倉庫名稱/', 
+  base: '/114-2/', 
   plugins: [vue()],
 })
